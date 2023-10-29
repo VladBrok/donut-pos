da1ff5193a91cbb674be192cbfcc8fb1ff54c1bc
VladBrok <vlad.brok99@gmail.com>
2023-10-29 19:42:23 +0300

    add logux server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,15 @@
-console.log("no");
+import { Server } from "@logux/server";
+
+const server = new Server(
+  Server.loadOptions(process, {
+    subprotocol: "1.0.0",
+    supports: "1.x",
+    fileUrl: import.meta.url,
+  })
+);
+
+server.auth(({ userId, token }) => {
+  return true;
+});
+
+server.listen();
