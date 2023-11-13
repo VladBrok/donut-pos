de124451205b3327e5aab90923f09c054df5371e
VladBrok <vlad.brok99@gmail.com>
2023-11-13 23:16:27 +0300

    add dish category actions and dish category module to server

--- backend/src/lib/db/schemas.ts
+++ backend/src/lib/db/schemas.ts
@@ -1,6 +1,8 @@
-import { employee, role } from "../../../migrations/schema.js";
+import { dishCategory, employee, role } from "../../../migrations/schema.js";
 
 export type SelectEmployeeSchema = {
   [employee._.name]: typeof employee.$inferSelect;
   [role._.name]: typeof role.$inferSelect | null;
 };
+
+export type DishCategorySchema = typeof dishCategory.$inferSelect;
