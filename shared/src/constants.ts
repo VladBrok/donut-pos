e255efa5823d42726ad55ed0b17c152f6227257a
VladBrok <vlad.brok99@gmail.com>
2023-11-18 12:09:03 +0300

    upload placeholder image to hosting

--- shared/src/constants.ts
+++ shared/src/constants.ts
@@ -2,6 +2,5 @@ export const ANONYMOUS = {
   userId: "anonymous",
 };
 
-// TODO: upload to hosting
 export const MISSING_PHOTO_PLACEHOLDER_URL =
-  "https://varonco.com/wp-content/uploads/2019/11/no-image-2.jpg";
+  "https://i.ibb.co/WxHxYtZ/no-image-2.jpg";
