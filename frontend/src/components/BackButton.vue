6f73d5a53541ee4392039bc7be1553aaa8b09ee3
VladBrok <vlad.brok99@gmail.com>
2023-12-02 10:40:40 +0100

    add reusable back button

---
+++ frontend/src/components/BackButton.vue
@@ -0,0 +1,20 @@
+<template>
+  <q-btn
+    class="q-mb-md"
+    flat
+    round
+    color="primary"
+    icon="arrow_back"
+    @click="$router.back()"
+  >
+    <q-tooltip>
+      {{ t.goBack }}
+    </q-tooltip>
+  </q-btn>
+</template>
+
+<script setup lang="ts">
+import { useI18nStore } from "../lib/i18n";
+
+const t = useI18nStore();
+</script>
