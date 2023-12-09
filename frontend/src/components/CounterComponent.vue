4aa66936534ae569d537938514632422cfd96fc8
VladBrok <vlad.brok99@gmail.com>
2023-12-09 09:23:01 +0100

    add counter component, clamp it to the bottom of the dish card

---
+++ frontend/src/components/CounterComponent.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="row items-center gap-sm">
+    <q-btn
+      padding="xs"
+      color="primary"
+      outline
+      icon="remove"
+      :disable="count === 0"
+      @click="emit('decrement')"
+    />
+    <div>
+      {{ count }}
+    </div>
+    <q-btn padding="xs" color="primary" icon="add" @click="emit('increment')" />
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{ count: number }>();
+
+const emit = defineEmits<{
+  increment: [];
+  decrement: [];
+}>();
+</script>
--- frontend/src/components/DishCard.vue
+++ frontend/src/components/DishCard.vue
@@ -1,30 +1,38 @@
 <template>
-  <q-card class="">
-    <q-card-section>
-      <q-img
-        :src="dish.imageUrl"
-        alt=""
-        fit="cover"
-        class="image-card shadow-3 rounded-borders"
-      />
-    </q-card-section>
-
-    <q-card-section class="row justify-between">
-      <div class="text-h6 q-mr-sm">{{ dish.name }}</div>
-      <div class="text-h6 text-primary">{{ formatCurrency(dish.price) }}</div>
-    </q-card-section>
-
-    <!-- TODO: for client, include description -->
-
-    <!-- TODO: display - 0 + (buttons and count, add to DRAFT order) -->
+  <q-card>
+    <div class="column no-wrap full-height">
+      <q-card-section>
+        <q-img
+          :src="dish.imageUrl"
+          alt=""
+          fit="cover"
+          class="image-card shadow-3 rounded-borders"
+        />
+      </q-card-section>
+      <q-card-section class="row justify-between">
+        <div class="text-h6 q-mr-sm">{{ dish.name }}</div>
+        <div class="text-h6 text-primary">{{ formatCurrency(dish.price) }}</div>
+      </q-card-section>
+      <q-card-section class="q-mt-auto">
+        <counter-component
+          :count="count"
+          @increment="count++"
+          @decrement="count--"
+        ></counter-component>
+      </q-card-section>
+    </div>
   </q-card>
 </template>
 
 <script setup lang="ts">
 import { loadDishesAction } from "donut-shared/src/actions/dishes";
+import { ref } from "vue";
 import { formatCurrency } from "../lib/format-currency";
+import CounterComponent from "./CounterComponent.vue";
 
 defineProps<{
   dish: ReturnType<typeof loadDishesAction>["payload"]["dishes"][number];
 }>();
+
+const count = ref(0);
 </script>
