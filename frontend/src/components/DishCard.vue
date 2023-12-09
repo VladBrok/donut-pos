cac7415913fe08ff68dec5b930702e200a0c0358
VladBrok <vlad.brok99@gmail.com>
2023-12-09 11:09:59 +0100

    extract ProductCard component

--- frontend/src/components/DishCard.vue
+++ frontend/src/components/DishCard.vue
@@ -1,43 +1,34 @@
 <template>
-  <q-card :class="{ 'shadow-0': expanded }">
-    <div class="column no-wrap full-height">
-      <q-card-section>
-        <q-img
-          :src="dish.imageUrl"
-          alt=""
-          fit="cover"
-          class="image-card shadow-3 rounded-borders"
-        />
-      </q-card-section>
-      <div class="row justify-between q-px-md">
-        <div class="text-h6 q-mr-sm">{{ dish.name }}</div>
-        <div class="text-h6 text-primary">{{ formatCurrency(dish.price) }}</div>
-      </div>
-      <q-card-section v-if="!expanded" class="q-mt-auto row justify-end">
-        <q-btn
-          color="primary"
-          icon-right="shopping_basket"
-          padding="0 sm"
-          @click="emit('addClick')"
-        >
-          <span class="text-h5 q-pr-sm">+</span>
-          <q-tooltip>
-            {{ t.addToCurrentOrder }}
-          </q-tooltip>
-        </q-btn>
-      </q-card-section>
-      <q-card-section v-if="expanded">
-        <div v-html="sanitizeHtml(dish.description)"></div>
-      </q-card-section>
-    </div>
-  </q-card>
+  <product-card
+    :no-shadow="expanded"
+    :image-url="dish.imageUrl"
+    :price="dish.price"
+    :name="dish.name"
+  >
+    <q-card-section v-if="!expanded" class="q-mt-auto row justify-end">
+      <q-btn
+        color="primary"
+        icon-right="shopping_basket"
+        padding="0 sm"
+        @click="emit('addClick')"
+      >
+        <span class="text-h5 q-pr-sm">+</span>
+        <q-tooltip>
+          {{ t.addToCurrentOrder }}
+        </q-tooltip>
+      </q-btn>
+    </q-card-section>
+    <q-card-section v-if="expanded">
+      <div v-html="sanitizeHtml(dish.description)"></div>
+    </q-card-section>
+  </product-card>
 </template>
 
 <script setup lang="ts">
 import { loadDishesAction } from "donut-shared/src/actions/dishes";
-import { formatCurrency } from "../lib/format-currency";
 import { useI18nStore } from "../lib/i18n";
 import { sanitizeHtml } from "../lib/sanitize-html";
+import ProductCard from "./ProductCard.vue";
 
 defineProps<{
   dish: ReturnType<typeof loadDishesAction>["payload"]["dishes"][number];
---
+++ frontend/src/components/ProductCard.vue
@@ -0,0 +1,30 @@
+<template>
+  <q-card :class="{ 'shadow-0': noShadow }">
+    <div class="column no-wrap full-height">
+      <q-card-section>
+        <q-img
+          :src="imageUrl"
+          alt=""
+          fit="cover"
+          class="image-card shadow-3 rounded-borders"
+        />
+      </q-card-section>
+      <div class="row justify-between q-px-md">
+        <div class="text-h6 q-mr-sm">{{ name }}</div>
+        <div class="text-h6 text-primary">{{ formatCurrency(price) }}</div>
+      </div>
+      <slot />
+    </div>
+  </q-card>
+</template>
+
+<script setup lang="ts">
+import { formatCurrency } from "../lib/format-currency";
+
+defineProps<{
+  price: number;
+  name: string;
+  imageUrl: string;
+  noShadow?: boolean;
+}>();
+</script>
