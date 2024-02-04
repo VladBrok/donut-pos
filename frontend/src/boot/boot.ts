ceb48d2f9dd7649b5f2003f236522fb5245c63e9
VladBrok <vlad.brok99@gmail.com>
2024-02-04 11:21:09 +0100

    install apex charts, add donut chart with mock data

--- frontend/src/boot/boot.ts
+++ frontend/src/boot/boot.ts
@@ -3,11 +3,13 @@ import customParseFormat from "dayjs/plugin/customParseFormat.js";
 import timezone from "dayjs/plugin/timezone.js";
 import utc from "dayjs/plugin/utc.js";
 import { boot } from "quasar/wrappers";
+import VueApexCharts from "vue3-apexcharts";
 
 // "async" is optional;
 // more info on params: https://v2.quasar.dev/quasar-cli/boot-files
-export default boot(async (/* { app, router, ... } */) => {
+export default boot(async ({ app, router }) => {
   dayjs.extend(customParseFormat);
   dayjs.extend(utc);
   dayjs.extend(timezone);
+  app.use(VueApexCharts);
 });
--- frontend/src/pages/admin/AdminDashboard.vue
+++ frontend/src/pages/admin/AdminDashboard.vue
@@ -1,5 +1,19 @@
 <template>
-  <div>dashboard</div>
+  <div>
+    <apexchart
+      width="380"
+      type="donut"
+      :options="options"
+      :series="series"
+    ></apexchart>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { computed } from "vue";
+
+const series = computed(() => [44, 55, 41]);
+const options = computed(() => ({
+  labels: ["Apple", "Mango", "Orange"],
+}));
+</script>
