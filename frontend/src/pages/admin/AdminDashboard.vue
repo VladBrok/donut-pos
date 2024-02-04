32e07ded2ecbf011411021fdf123882a357f5f02
VladBrok <vlad.brok99@gmail.com>
2024-02-04 11:03:43 +0100

    add dashboard page

--- frontend/src/pages/admin/AdminDashboard.vue
+++ frontend/src/pages/admin/AdminDashboard.vue
@@ -1,6 +1,5 @@
 <template>
-  <!-- TODO: show stats. It should be a separate page with stats available on menu. Redirect to it from this page -->
-  <h1 class="text-center">Welcome</h1>
+  <div>dashboard</div>
 </template>
 
 <script setup lang="ts"></script>
