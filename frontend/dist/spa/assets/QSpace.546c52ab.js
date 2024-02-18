701a310bd6122b4c31d57e9bc3d2bc167ec162fb
VladBrok <vlad.brok99@gmail.com>
2024-02-18 16:30:52 +0100

    update

--- frontend/dist/spa/assets/QSpace.546c52ab.js
+++
@@ -1,1 +0,0 @@
-import{q as a,p as e}from"./index.080b44f7.js";const p=a("div",{class:"q-space"});var c=e({name:"QSpace",setup(){return()=>p}});export{c as Q};
---
+++ frontend/dist/spa/assets/QSpace.b02b7570.js
@@ -0,0 +1,1 @@
+import{q as a,p as e}from"./index.20f37243.js";const p=a("div",{class:"q-space"});var c=e({name:"QSpace",setup(){return()=>p}});export{c as Q};
